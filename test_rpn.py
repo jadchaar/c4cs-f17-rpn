d83aaec0608981f77d4c38cfe62403dcf641521d
jadchaar <jad.chaar@gmail.com>
2017-10-27 13:27:40 -0400

    added a failing exponentiation test for travis status

--- test_rpn.py
+++ test_rpn.py
@@ -11,3 +11,7 @@ class TestBasics(unittest.TestCase):
     def test_subtract(self):
         result = rpn.calculate("5 3 -")
         self.assertEqual(2, result)
+
+    def test_exponentiation(self):
+        result = rpn.calculate("2 3 ^")
+        self.assertEqual(8, result)
