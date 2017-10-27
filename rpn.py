1d334e353cc4c53d3afa29c9db1cc07c382aa0ea
jadchaar <jad.chaar@gmail.com>
2017-10-27 17:42:52 -0400

    finished advanced assignment

--- rpn.py
+++ rpn.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+import readline  # noqa: F401
 import operator
+from termcolor import colored
 
 
 ops = {
@@ -17,8 +19,14 @@ def calculate(arg):
         except ValueError:
             arg2 = stack.pop()
             arg1 = stack.pop()
+            to_print = str(arg1) + " " + colored(token, "magenta") + " " + str(arg2)
             function = ops[token]
             result = function(arg1, arg2)
+            if result < 0:
+                to_print += " = " + colored(str(result), "red")
+            else:
+                to_print += " = " + colored(str(result), "cyan")
+            print(to_print)
             stack.append(result)
     return stack.pop()
 
@@ -31,8 +39,18 @@ def reduce_coverage():
 
 
 def main():
+    total_input = ""
+    print("Type = to calculate")
     while True:
-        calculate(input("rpn calc> "))
+        user_input = input("rpn calc> ")
+        if user_input == "=":
+            break
+        else:
+            total_input += str(user_input) + " "
+
+    print("Calculating result...")
+    calculate(total_input)
+
 
 
 if __name__ == '__main__':
